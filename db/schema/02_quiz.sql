01ebe2c17c8ef050d7d203b0e9665eebe7b4e009
Di <dwu233@gmail.com>
2022-05-07 17:02:33 +0000

    add table for users and quizzes

--- db/schema/02_quiz.sql
+++ db/schema/02_quiz.sql
@@ -1,13 +1,3 @@
--- Drop and recreate Users table (Example)
-
-DROP TABLE IF EXISTS user CASCADE;
-CREATE TABLE user (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE quiz (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
