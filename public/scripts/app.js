4c18269d7ea3b791bc04b6be2ee4c00b91f86c7e
Heron Feijao <heronfeijao@gmail.com>
2022-05-07 15:33:07 -0400

    organized index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,8 +10,16 @@ $(document).ready(() => {
     }
   });
 
-  $('#scroll-top').click(() => {
+  $('#scroll-top').on('click', () => {
     $(document).scrollTop(0);
   });
+
+  $('.clipboard').on('click', function() {
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $("p").text("URL copied!");
+  })
 
 });
