b91c712e5e562a2abca5cc3faf01bc4d2cd6dd60
Heron Feijao <heronfeijao@gmail.com>
2022-05-07 11:35:25 -0400

    fixing layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,17 @@
 // Client facing scripts here
+
+$(document).ready(() => {
+
+  $(document).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll-top').fadeIn();
+    } else {
+      $('#scroll-top').fadeOut();
+    }
+  });
+
+  $('#scroll-top').click(() => {
+    $(document).scrollTop(0);
+  });
+
+});
