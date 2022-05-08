0397491f0c0ff5f51d03d4e43029982897460d0e
lamew128 <lamew128@gmail.com>
2022-05-07 22:55:09 -0400

    username password input test

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name,email,password) VALUES ('Heron','heron@heron.com','password');
 INSERT INTO users (name,email,password) VALUES ('Eugene','eugene@eugene.com','password');
+INSERT INTO users (name,email,password) VALUES ('user1','1@1','1');
 
