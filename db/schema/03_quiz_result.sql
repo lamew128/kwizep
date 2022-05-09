e0993d0841db9184bf50626d9d5f05be7207bb2c
Di <dwu233@gmail.com>
2022-05-09 06:53:15 +0000

    remove a non-unique reference from 03_quiz.sql

--- db/schema/03_quiz_result.sql
+++ db/schema/03_quiz_result.sql
@@ -5,14 +5,13 @@ DROP TABLE IF EXISTS quiz_result CASCADE;
 CREATE TABLE quiz_take (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quiz(id),
-  quiz_answers_id INTEGER REFERENCES quiz_answers_id,
-  quiz_answers_correct BOOLEAN REFERENCES quiz_answers(correct)
+  quiz_answers_id INTEGER REFERENCES quiz_answers_id ON DELETE CASCADE,
 );
 
 CREATE TABLE quiz_result (
   id SERIAL PRIMARY KEY NOT NULL,
   taker_id INTEGER REFERENCES user(id),
   quiz_take_id INTEGER REFERENCES quiz_take(id),
-  score INTEGER,
+  score INTEGER NOT NULL,
   url NOT NULL
 );
